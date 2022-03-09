60b69887ae0724723799608541654c2c19165ade
andyswj <e0696682@u.nus.edu>
2022-03-09 09:25:47 +0800

    new

---
+++ src/test/java/edu/nus/iss/Revision_2/Revision2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nus.iss.Revision_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Revision2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
